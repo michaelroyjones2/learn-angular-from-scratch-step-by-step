3b7697755a69afec8009f642d9d7d6ad0776d506
michaelroyjones2 <michaelroyjones11@outlook.com>
2022-06-12 22:25:02 -0400

    final project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://767dr1o8j0.execute-api.us-east-1.amazonaws.com'
 };
